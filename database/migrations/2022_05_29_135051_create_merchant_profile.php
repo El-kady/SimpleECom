a61e9ffc7aba0dd6ac49fe33d4d2a2bc1e915315
Mostafa Elkady <mostafa.elkady@vodafone.com>
2022-05-29 16:25:42 +0200

    add user_type and merchant profile table

---
+++ database/migrations/2022_05_29_135051_create_merchant_profile.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMerchantProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('merchant_profile', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("user_id");
+
+            $table->string('store_name');
+            $table->integer("shipping_cost");
+            $table->boolean("vat_included");
+            $table->integer("vat_percentage");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('merchant_profile');
+    }
+}
