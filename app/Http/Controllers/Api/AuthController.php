075f3b18847f76680e2ca9070ec78dc19fbb4983
Mostafa Elkady <mostafa.elkady@vodafone.com>
2022-05-29 18:20:09 +0200

    add login&register using passport

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Exception;
+
+class AuthController extends Controller
+{
+    use ApiResponser;
+    /**
+     * User login API method
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) return $this->sendError('Validation Error.', $validator->errors(), 422);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user             = Auth::user();
+            $success['name']  = $user->name;
+            $success['token'] = $user->createToken('accessToken')->accessToken;
+
+            return $this->sendResponse($success, 'You are successfully logged in.');
+        } else {
+            return $this->sendError('Unauthorised', ['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    /**
+     * User registration API method
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required',
+            'email'    => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'user_type' => 'required|in:MERCHANT,CONSUMER'
+        ]);
+
+        if ($validator->fails()) return $this->sendError('Validation Error.', $validator->errors(), 422);
+
+        try {
+
+            $user = User::create([
+                'name'     => $request->name,
+                'email'    => $request->email,
+                'password' => bcrypt($request->password),
+                'user_type' => $request->user_type,
+            ]);
+
+            $success['name']  = $user->name;
+            $message          = 'A user has been successfully created.';
+            $success['token'] = $user->createToken('accessToken')->accessToken;
+        } catch (Exception $e) {
+            $success['token'] = [];
+            $message          = 'Oops! Unable to create a new user.';
+        }
+
+        return $this->sendResponse($success, $message);
+    }
+}
---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+use Carbon\Carbon;
+
+trait ApiResponser
+{
+
+    function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        !empty($errorMessages) ? $response['data'] = $errorMessages : null;
+
+        return response()->json($response, $code);
+    }
+
+}
